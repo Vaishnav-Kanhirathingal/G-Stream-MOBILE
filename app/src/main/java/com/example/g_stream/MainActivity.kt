b00ea834cf28a1845c6a8b6e11c2c0007edcd860
Vaishnav Kanhirathingal <vaishnav.kanhira@gmail.com>
2022-07-25 20:54:31 +0530

    full screen activity added

--- app/src/main/java/com/example/g_stream/MainActivity.kt
+++ app/src/main/java/com/example/g_stream/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.g_stream
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.g_stream.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        applyBinding()
+    }
+
+    private fun applyBinding() {
+        binding.testButton.setOnClickListener {
+            // TODO: go to full screen
+            startActivity(Intent(this,StreamActivity::class.java))
+        }
     }
 }
