29d96368c50b00368a50c7190b72809118b7ef71
Vaishnav Kanhirathingal <vaishnav.kanhira@gmail.com>
2022-07-28 11:10:44 +0530

    preview functional

--- app/src/main/java/com/example/g_stream/ui_elements/fragments/ScanFragment.kt
+++ app/src/main/java/com/example/g_stream/ui_elements/fragments/ScanFragment.kt
@@ -3,16 +3,29 @@ package com.example.g_stream.ui_elements.fragments
 import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.camera.core.AspectRatio
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.example.g_stream.databinding.FragmentScanBinding
+import com.example.g_stream.viewmodel.StreamViewModel
+
+private const val TAG = "ScanFragment"
 
 class ScanFragment : Fragment() {
     private lateinit var binding: FragmentScanBinding
+    private lateinit var cameraSelector: CameraSelector
+    private lateinit var cameraProvider: ProcessCameraProvider
+    private lateinit var previewUseCase: Preview
+    private val screenAspectRatio = AspectRatio.RATIO_4_3
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,22 +45,57 @@ class ScanFragment : Fragment() {
         }
     }
 
+    private fun isCameraPermissionGranted(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            requireContext(),
+            Manifest.permission.CAMERA
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentScanBinding.inflate(layoutInflater)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        applyBinding()
+        setupCamera()
     }
 
-    private fun applyBinding() {
-        // TODO: perform scanning
-        binding.cameraPreview
+    private fun setupCamera() {
+        val lensFacing = CameraSelector.LENS_FACING_BACK
+        cameraSelector = CameraSelector.Builder().requireLensFacing(lensFacing).build()
+
+        ViewModelProvider(
+            this, ViewModelProvider.AndroidViewModelFactory.getInstance(this.activity!!.application)
+        )[StreamViewModel::class.java]
+            .processCameraProvider
+            .observe(viewLifecycleOwner) {
+                cameraProvider = it
+                if (isCameraPermissionGranted()) bindCameraUseCases() else getPermissions()
+            }
+    }
+
+    private fun bindCameraUseCases() {
+        previewUseCase = Preview.Builder()
+            .setTargetAspectRatio(screenAspectRatio)
+            .setTargetRotation(binding.cameraPreview.display.rotation)
+            .build()
+        previewUseCase.setSurfaceProvider(binding.cameraPreview.surfaceProvider)
+        try {
+            cameraProvider.bindToLifecycle(
+                this,
+                cameraSelector,
+                previewUseCase
+            )
+        } catch (illegalStateException: IllegalStateException) {
+            Log.e(TAG, illegalStateException.message!!)
+        } catch (illegalArgumentException: IllegalArgumentException) {
+            Log.e(TAG, illegalArgumentException.message!!)
+        }
     }
 
     private fun onSuccess() {
