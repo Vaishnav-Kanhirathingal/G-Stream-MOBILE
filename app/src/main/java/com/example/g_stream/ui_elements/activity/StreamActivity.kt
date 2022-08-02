f237849f09fc4155c7d0a934066ed9b9abed0c51
Vaishnav Kanhirathingal <vaishnav.kanhira@gmail.com>
2022-08-02 20:38:44 +0530

    stream activity is now permanently full screen and landscape mode

--- app/src/main/java/com/example/g_stream/ui_elements/activity/StreamActivity.kt
+++ app/src/main/java/com/example/g_stream/ui_elements/activity/StreamActivity.kt
@@ -1,6 +1,5 @@
 package com.example.g_stream.ui_elements.activity
 
-import android.annotation.SuppressLint
 import android.os.Build
 import android.os.Bundle
 import android.os.Handler
@@ -8,7 +7,6 @@ import android.os.Looper
 import android.util.Log
 import android.view.View
 import android.view.WindowInsets
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.g_stream.connection.ConnectionData
 import com.example.g_stream.databinding.ActivityStreamBinding
@@ -17,81 +15,34 @@ class StreamActivity : AppCompatActivity() {
     private val TAG = this::class.java.simpleName
 
     private lateinit var binding: ActivityStreamBinding
-    private lateinit var fullscreenContent: TextView
     private val hideHandler = Handler(Looper.myLooper()!!)
 
-    private val hidePart2Runnable = Runnable {
-        if (Build.VERSION.SDK_INT >= 30) {
-            fullscreenContent.windowInsetsController?.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
-        } else {
-            fullscreenContent.systemUiVisibility =
-                View.SYSTEM_UI_FLAG_LOW_PROFILE or
-                        View.SYSTEM_UI_FLAG_FULLSCREEN or
-                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
-                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
-                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-        }
-    }
-    private val showPart2Runnable = Runnable { supportActionBar?.show() }
-    private var isFullscreen: Boolean = false
-
-    private val hideRunnable = Runnable { hide() }
-
-    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         val connectionData = intent.getStringExtra(ConnectionData.key)
         Log.d(TAG, "data received = $connectionData!!")
 
         binding = ActivityStreamBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        applyBinding()
+    }
+
+    private fun applyBinding() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        isFullscreen = true
-        fullscreenContent = binding.fullscreenContent
-        fullscreenContent.setOnClickListener { toggle() }
-    }
-
-    override fun onPostCreate(savedInstanceState: Bundle?) {
-        super.onPostCreate(savedInstanceState)
-        delayedHide(100)
-    }
-
-    private fun toggle() {
-        if (isFullscreen) {
-            hide()
-        } else {
-            show()
-        }
-    }
-
-    private fun hide() {
         supportActionBar?.hide()
-        isFullscreen = false
-        hideHandler.removeCallbacks(showPart2Runnable)
-        hideHandler.postDelayed(hidePart2Runnable, UI_ANIMATION_DELAY)
-    }
-
-    private fun show() {
-        if (Build.VERSION.SDK_INT >= 30) {
-            fullscreenContent.windowInsetsController?.show(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
-        } else {
-            fullscreenContent.systemUiVisibility =
-                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
-                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-        }
-        isFullscreen = true
-        hideHandler.removeCallbacks(hidePart2Runnable)
-        hideHandler.postDelayed(showPart2Runnable, UI_ANIMATION_DELAY)
-    }
-
-    private fun delayedHide(delayMillis: Int) {
-        hideHandler.removeCallbacks(hideRunnable)
-        hideHandler.postDelayed(hideRunnable, delayMillis.toLong())
-    }
-
-    companion object {
-        private const val UI_ANIMATION_DELAY = 300L
+        hideHandler.removeCallbacks { supportActionBar?.show() }
+        hideHandler.postDelayed({
+            if (Build.VERSION.SDK_INT >= 30) {
+                binding.fullscreenContent.windowInsetsController?.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+            } else {
+                binding.fullscreenContent.systemUiVisibility =
+                    View.SYSTEM_UI_FLAG_LOW_PROFILE or
+                            View.SYSTEM_UI_FLAG_FULLSCREEN or
+                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+            }
+        }, 300L)
     }
 }
